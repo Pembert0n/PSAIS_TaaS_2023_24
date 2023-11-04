8d1fdba165babde55a36f4fe4df717ba7044440d
Rolvo <suphi.pembe@gmail.com>
2023-11-04 13:30:17 +0100

    delete alive

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import whisper
-print("alive1")
+
 model = whisper.load_model("base")
-print("alive2")
 result = model.transcribe("podcast/knowledge_science_ep1.mp3")
-print("alive3")
+
 with open("transcription.txt", "w") as f:
-    print("alive4")
     f.write(result["text"])
 print("done")
