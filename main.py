1036f68542d70aba60559c254155cffe25c4ac63
spembe <spembe@stud.hs-heilbronn.de>
2023-12-08 14:56:53 +0100

    whisper_timestamped 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15 13 15

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-import whisper
+import whisper as whisper_OG
+import whisper_timestamped as whisper
+import json
+#code ausschnitt geliehen von https://github.com/linto-ai/whisper-timestamped?tab=readme-ov-file#installation
+audio = whisper.load_audio("podcast/knowledge_science_ep1.mp3")
 
-model = whisper.load_model("base")
-result = model.transcribe("podcast/knowledge_science_ep1.mp3")
+model = whisper.load_model("tiny", device="cpu")
 
-with open("transcription.txt", "w") as f:
-    f.write(result["text"])
-print("done")
+result = whisper.transcribe(model, audio, language="de")
+
+
+print(json.dumps(result, indent = 2, ensure_ascii = False))
+
+
+# model = whisper.load_model("base")
+# result = model.transcribe("podcast/knowledge_science_ep1.mp3")
+
+# with open("transcription.txt", "w") as f:
+#     f.write(result["text"])
+# print("done")
