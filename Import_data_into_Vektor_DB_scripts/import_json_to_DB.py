45ea2f205d872cf8b5b9fe968fb4a3910408f458
Au-Thanh Duong <aduong@stud.hs-heilbronn.de>
2024-01-10 14:53:19 +0100

    update

--- Import_data_into_Vektor_DB_scripts/import_json_to_DB.py
+++ Import_data_into_Vektor_DB_scripts/import_json_to_DB.py
@@ -1,9 +1,13 @@
 #For Row based design (transcription.txt ist zwar keine json aber das ist wahrscheinlich das passende)
 #wir können das von txt auf json ohne probleme ändern --suphi
+from pymilvus import connections
+
+# Connect to Milvus Docker container
+connections.connect(host='localhost', port='19530')
 
 from pymilvus import utility
 task_id = utility.do_bulk_insert(
     collection_name="Podcast",
     partition_name="2023",
-    files=["../transcription.json"] #For a row-based JSON file, parameter files should be a one-member list containing the path to the JSON file.
+    files=["../knowledge_science_ep1_transcription.json"] #For a row-based JSON file, parameter files should be a one-member list containing the path to the JSON file.
 )
